4ee8dd742ee78dd0ee1a983bb47b18f96222cfc9
JesusMejia3VC51 <75956851+JesusMejia3VC51@users.noreply.github.com>
2021-01-14 17:08:11 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+    apiKey: "AIzaSyASafTUcdu1jyQ_tGBdJzbo1ikg4sqYq1E",
+    authDomain: "proyectofinal-jesusmejia.firebaseapp.com",
+    projectId: "proyectofinal-jesusmejia",
+    storageBucket: "proyectofinal-jesusmejia.appspot.com",
+    messagingSenderId: "739200163164",
+    appId: "1:739200163164:web:47504f96ffee9c28b53599",
+    measurementId: "G-XJ9WR7G8NG"
+  });
+ 
